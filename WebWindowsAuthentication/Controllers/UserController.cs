324a7a2557b60874855305406172c2254e689bed
marcOcram <marcOcram@users.noreply.github.com>
2020-08-21 09:28:13 +0200

    Simple Web Windows Authentication

---
+++ WebWindowsAuthentication/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebWindowsAuthentication.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ViewBag.IsPost = false;
+            ViewBag.UserIdentityName = User?.Identity?.Name;
+            ViewBag.ContextUserIdentityName = HttpContext?.User?.Identity?.Name;
+
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult IndexPost()
+        {
+            ViewBag.IsPost = true;
+            ViewBag.UserIdentityName = User?.Identity?.Name;
+            ViewBag.ContextUserIdentityName = HttpContext?.User?.Identity?.Name;
+
+            return View();
+        }
+    }
+}
